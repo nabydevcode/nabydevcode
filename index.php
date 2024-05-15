577b3c82046051c7bebdfd07f4f01217b332e454
nabydevcode <condenabytoure123@gmail.com>
2024-05-15 23:00:26 +0200

    condition

--- index.php
+++ index.php
@@ -13,5 +13,37 @@ $eleves = [
     ],
 ];
 
-echo "Bonjour " . ' ' . $eleves[1]['nom'] . ' ' . $eleves[1]['prenoms'] . ' ' . 'vous aviez eu ' . ' ' . (($eleves[1]['notes'][0] + $eleves[1]['notes'][1] + $eleves[1]['notes'][2]) / 3) . ' ' . 'de moyenne';
+//echo "Bonjour " . ' ' . $eleves[1]['nom'] . ' ' . $eleves[1]['prenoms'] . ' ' . 'vous aviez eu ' . ' ' . (($eleves[1]['notes'][0] + $eleves[1]['notes'][1] + $eleves[1]['notes'][2]) / 3) . ' ' . 'de moyenne';
 
+// les creneaux d'ouverture 
+
+$heure = (int) readline('donner vore darriver : ');
+if (!((9 <= $heure && $heure <= 12) || (14 <= $heure && $heure <= 17))) {
+    echo " la pharmacie est fermé ";
+} else {
+
+    echo " la pharmacie est ouverte";
+}
+/* $matiere = (string) readline('quelle matiere :');
+$notes1 = (int) readline('note1: ');
+$notes2 = (int) readline('note2: ');
+
+$moyenne = (int) (($notes1 + $notes2)) / 2;
+
+if ($moyenne >= 10) {
+    echo " informations \n";
+    echo "Matiere: $matiere \n";
+    echo "Notes1: $notes1 \n";
+    echo "Notes2: $notes2 \n";
+    echo "Moyenne: $moyenne \n";
+    echo "vous avez la moyenne ";
+} elseif ($moyenne < 10) {
+    echo " informations";
+    echo "Matiere: $matiere \n";
+    echo "Notes1: $notes1 \n";
+    echo "Notes2: $notes2 \n";
+    echo "Moyenne: $moyenne \n";
+    echo " vous n'aviez pas la moyenne : $moyenne  ";
+} elseif ($moyenne == 0) {
+    echo "la valeur inconnu";
+} */
