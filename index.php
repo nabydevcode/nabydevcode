e671a121e8c03ed559ec0ce2c4973febde114f01
nabydevcode <condenabytoure123@gmail.com>
2024-05-15 20:20:34 +0200

    les tableaux

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
-$nom = "marc";
-$prenoms = " doe";
-$note1 = 12;
-$note2 = 13;
+$eleves = [
+    [
 
-$moyenne = ($note1 + $note2) / 2;
+        'nom' => 'toure',
+        'prenoms' => 'naby zakaria',
+        'notes' => [16, 16, 20],
+    ],
+    [
+        'nom' => 'conde',
+        'prenoms' => 'mohamed lamine',
+        'notes' => [18, 17, 20],
+    ],
+];
 
-echo 'Bonjour ' . $nom . ' ' . $prenoms . ' vous aviez eu ' . ' ' . $moyenne . ' ' . 'moyennne';
+echo "Bonjour " . ' ' . $eleves[1]['nom'] . ' ' . $eleves[1]['prenoms'] . ' ' . 'vous aviez eu ' . ' ' . (($eleves[1]['notes'][0] + $eleves[1]['notes'][1] + $eleves[1]['notes'][2]) / 3) . ' ' . 'de moyenne';
+
