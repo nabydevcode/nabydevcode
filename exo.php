fe06de982642a824129066efbff90392f619cc21
nabydevcode <condenabytoure123@gmail.com>
2024-05-18 22:35:59 +0200

    entrainnement sur les fonctions

--- exo.php
+++ exo.php
@@ -1,7 +1,7 @@
 <?php
 // deviner les mots palyndrom
 
-$mots = (string) readline(" donner un mot palyndron :");
+/* $mots = (string) readline(" donner un mot palyndron :");
 if (preg_match('/[A-Z]/', $mots) === 1) {
     echo " ce mots contient un des majuscule ";
 } else {
@@ -12,6 +12,29 @@ if (preg_match('/[A-Z]/', $mots) === 1) {
         echo " ce mots n'est pas paylyndrom\n";
     }
 
+} */
+//recherce  les mots d'insulte 
+
+$insultes = ['con', 'merde'];
+$message = (string) readline("donner une phrase :");
+$contientInsulte = false;
+
+foreach ($insultes as $insulte) {
+    if (strpos($message, $insulte) !== false) {
+        $contientInsulte = true;
+        break;
+    }
 }
+if ($contientInsulte) {
+    echo " cette phrase contient une insulte ";
+} else {
+    echo " ce mots ne contient pas des insulte (: \n";
+}
+
+
+
+
+
+
 
 
