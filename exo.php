60a79d798e8232482d140384e571318dd4939098
nabydevcode <condenabytoure123@gmail.com>
2024-05-18 23:29:59 +0200

    les exerices

--- exo.php
+++ exo.php
@@ -17,11 +17,10 @@ if (preg_match('/[A-Z]/', $mots) === 1) {
 
 $insultes = ['con', 'merde', 'pute'];
 foreach ($insultes as $insulte) {
-    $changer[] = str_repeat('*', strlen($insulte));
+    $changer[] = $insulte[0] . str_repeat('*', strlen($insulte) - 1);
 }
 $message = (string) readline("donner une phrase :");
-
-$replace = str_replace($insultes, $changer, $message);
+$replace = str_replace($insultes, $changer, $message, );
 echo "$replace \n";
 
 
