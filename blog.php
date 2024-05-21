1ec3c8854b8718b57757ada23b9edcc31f524f06
nabydevcode <condenabytoure123@gmail.com>
2024-05-21 19:21:24 +0200

    formulaires

--- blog.php
+++ blog.php
@@ -1,23 +1,13 @@
 <?php
+/* $parfums = [
+    'frais' => 3,
+    'chocolat' => 5,
+    'vanille' => 6
+]; */
 
-$succes = "";
-$perdu = "";
-$magique = 60;
-
-$valeur = $_GET['numero'];
-$valeur = $_POST['numro'];
-
-if (isset($valeur) && !empty($valeur)) {
-    if ($magique === (int) $valeur) {
-        $succes = " Bravo vous aviez trouver le nombre magique";
-    } else {
-        $perdu = " Desolé vous aviez perdu Resayer encore ";
-    }
-} else {
-    echo "desolé";
-}
 
 require ('header.php');
+require ('technique/traite.php');
 
 ?>
 
@@ -26,22 +16,46 @@ require ('header.php');
     le jeux de devinette
 </h1>
 
-<?php if (isset($succes) && !empty($succes)): ?>
-    <div class="alert alert-success">
-        <?= $succes ?>
-    </div>
-<?php elseif ($perdu): ?>
-    <div class="alert alert-danger">
-        <?= $perdu ?>
-    </div>
-<?php endif ?>
+
 </div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-8">
-            <form action="#" methode="post">
-                <input type="number" name="numero" class="form-control mb-2" placeholder="0-200"
-                    value="<?php echo htmlspecialchars($valeur) ?>">
+            <form action="#" method="GET">
+                <h4> Parfums:</h4>
+                <?php foreach ($parfums as $parfum => $prix): ?>
+                    <div class="checkbox">
+                        <label>
+                            <?= checkbox('parfum', $parfum, $_GET); ?>
+                            <?= $parfum ?> -
+                            <?= $prix ?> €
+                        </label>
+                    </div>
+
+                <?php endforeach ?>
+                <h4> Cornets:</h4>
+                <?php foreach ($cornets as $cornet => $prix): ?>
+                    <div class="checkbox">
+                        <label>
+                            <?= radio('cornet', $cornet, $_GET); ?>
+                            <?= $cornet ?> -
+                            <?= $prix ?> €
+                        </label>
+                    </div>
+
+                <?php endforeach ?>
+
+                <h4> supplement :</h4>
+                <?php foreach ($supplements as $supplement => $prix): ?>
+                    <div class="checkbox">
+                        <label>
+                            <?= checkbox('supplement', $supplement, $_GET); ?>
+                            <?= $supplement ?> -
+                            <?= $prix ?> €
+                        </label>
+                    </div>
+
+                <?php endforeach ?>
                 <button type="submit" class="btn btn-primary">envoyer</button>
             </form>
         </div>
@@ -49,20 +63,20 @@ require ('header.php');
 </div>
 
 
-<?php
-echo "$-GET \n";
-print_r($_GET['numero']) ?>
-
-<?php
-echo "\n"
-    ?>
-<?php
 
 
+<pre>
 
-echo "$-POST \n";
-print_r($_POST['numero'])
-    ?>
+  <?php
+  var_dump($_GET);
+  ?>
+</pre>
+<pre>
+
+  <?php
+  var_dump($_POST);
+  ?>
+</pre>
 <?php
 
 require ('footer.php');
--- technique/traite.php
+++ technique/traite.php
@@ -1,10 +1,47 @@
 <?php
+$parfums = [
+    'frais' => 3,
+    'chocolat' => 5
+    ,
+    'vanile' => 6
+];
 
-$parfums = ['frais', 'chocolat', 'vanile'];
+$cornets = [
+    'pot' => 2,
+    'cornet' => 3,
+];
+$supplements = [
+    'pepite de chocolate' => 1,
+    'chantilly' => 0.5
 
-function cheicbook()
+];
+
+
+function checkbox(string $name, string $value, array $data): string
 {
+    $attributes = '';
+    if (isset($data['name']) && (in_array($value, $data['name']))) {
+        $attributes = "checked";
+    }
 
+    return <<<HTML
+    <input type="checkbox" name="{$name}[]" value="$value" $attributes>
+   HTML;
 
 }
+function radio(string $name, string $value, array $data): string
+{
+    $attributes = '';
+    if (isset($data['name']) && (in_array($value, $data['name']))) {
+
+        $attributes = 'checked';
+    }
+    return <<<HTML
+    <input type="checkbox" name="{$name}[]" value="$value" $attributes>
+   HTML;
+}
+
+
+
+
 
