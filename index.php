974bb82abfaa8bcba8f13e888bc0e8c0f0ec3faf
nabydevcode <condenabytoure123@gmail.com>
2024-05-28 21:25:34 +0200

    les fichiers

--- index.php
+++ index.php
@@ -2,18 +2,57 @@
 $title = "home";
 $nav = "index";
 require ('header.php');
+$error = "";
+if (isset($_POST['emails']) && !empty($_POST['emails'])) {
+
+    $email = filter_var($_POST['emails'], FILTER_VALIDATE_EMAIL);
+    $file = __DIR__ . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . date('m-d-y') . '.txt';
+    file_put_contents($file, $email . PHP_EOL, FILE_APPEND);
+
+}
 ?>
 
-<!-- Begin page content -->
-<main class="flex-shrink-0 mt-4">
-    <div class="container">
-        <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-        <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and
-            CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code
-                class="small">main &gt; .container</code>.</p>
-        <p>Back to <a href="/docs/5.3/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <h1> S'inscrire a Newletter
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nihil, commodi hic itaque asperiores in
+                officiis
+                excepturi accusamus. Quaerat necessitatibus maiores ab consequuntur fugiat, minus ad distinctio
+                exercitationem
+                cupiditate omnis.
+                Nobis voluptatem itaque suscipit dignissimos magni quod qui iusto esse blanditiis culpa voluptate, sunt
+                explicabo
+                dolorem placeat pariatur odit cum vel eligendi, repellendus impedit. Fugit animi quos et aut neque!
+                Quae dolor quod asperiores suscipit harum eaque tempora corporis officiis maxime nisi! Ratione sapiente
+                earum
+                placeat deleniti! Ullam, officia. Debitis maiores eligendi molestiae a laudantium dolores iste ut modi
+                repellendus.
+                Voluptate repudiandae, blanditiis enim dicta nemo nulla porro, distinctio debitis, quaerat repellat
+                animi rerum
+
+            </p>
+
+            <?php if ($email): ?>
+                <div class="alert    alert-success">
+                    Votre email est validé
+                </div>
+            <?php else: ?>
+                <div class="alert  alert-danger">
+                    Votre email est validé
+                </div>
+            <?php endif ?>
+            <form action="" method="POST">
+                <input type="email" name="emails" placeholder="exemple@.com">
+                <button type="submit" class="btn  btn-primary ">envoyer</button>
+            </form>
+        </div>
     </div>
-</main>
+</div>
+
 
 <footer class="footer mt-auto py-3 bg-body-tertiary">
     <div class="container">
