154800c032643d6220799feebba597eea17e3127
nabydevcode <condenabytoure123@gmail.com>
2024-06-02 19:10:14 +0200

    les fichiers

--- generale.php
+++ generale.php
@@ -3,48 +3,51 @@
 $title = "generale";
 require ('technique/traite.php');
 require ('header.php');
+require ('technique/crenneaux.php');
 $nombre = reuperer_les_vues();
-$annee_selction = (int) $_GET['annee'];
-
-
-$year = (int) date('y');
-$annee = (int) ('20' . $year);
-
-
-
+$year = (int) date('Y');
+$mois = date('m');
+$mois_selection = empty($_GET['mois']) ? '' : (int) $_GET['mois'];
+$annee_selction = empty($_GET['annee']) ? '' : (int) $_GET['annee'];
 ?>
+<div class="container">
+    <h1> Deshabord de mon site</h1>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card" style="width: 20rem;">
+                <div class="list-group  text-center">
+                    <?php for ($i = 0; $i < 5; $i++): ?>
+                        <a class="list-group-item <?= $year - $i === $annee_selction ? 'active' : ''; ?>"
+                            href="generale.php?annee=<?= $year - $i ?>">
+                            <?= $year - $i ?>
+                        </a>
+                        <?php if ($year - $i === $annee_selction): ?>
+                            <div class="list-group">
+                                <?php foreach (MOIS as $key => $value): ?>
 
-<h1> Deshabord de mon site</h1>
-<div class="row">
-    <div class="col-md-4">
-
-        <div class="card">
-            <div class="list-group ">
-                <?php for ($i = 0; $i < 5; $i++): ?>
-                    <a class="list-group-item <?= $annee === $annee_selction ? 'active' : '' ?>"
-                        href="generale.php?annee=<?= $annee - $i ?>" <?= $annee - $i ?>> </a>
-                <?php endfor ?>
+                                    <a class="list-group-item <?= $mois === $mois_selection ? 'active' : ''; ?> "
+                                        href="generale.php?annee=<?= $annee_selction ?>& mois= <?= $key ?>">
+                                        <?= $value ?>
+                                    </a>
+                                <?php endforeach ?>
+                            </div>
+                        <?php endif ?>
+                    <?php endfor ?>
+                </div>
             </div>
         </div>
-
-    </div>
-    <div class=" col-md-8">
-        <div class="card">
-            <div class="card-body">
-
-                <strong style="font-size: 3rem;">
-                    <?= $nombre ?>
-                </strong> <br>
-                Visite
-                <?= $nombre > 1 ? 's' : '' ?> total
-
+        <div class=" col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <strong style="font-size: 3rem;">
+                        <?= $nombre ?>
+                    </strong> <br>
+                    Visite
+                    <?= $nombre > 1 ? 's' : '' ?> total
+                </div>
             </div>
         </div>
-
-
     </div>
-
-
 </div>
 
 
--- technique/crenneaux.php
+++ technique/crenneaux.php
@@ -24,3 +24,23 @@ define(
         'Dimanche'
     ]
 );
+
+define(
+    'MOIS',
+    [
+        '01' => 'janvier',
+        '02' => 'fevrier',
+        '03' => 'mars',
+        '04' => 'avril',
+        '05' => 'mai',
+        '06' => 'juin',
+        '07' => 'juillet',
+        '08' => 'août',
+        '09' => 'setpembre',
+        '10' => 'octobre',
+        '11' => 'novembre',
+        '12' => 'decembre',
+    ]
+
+
+);
